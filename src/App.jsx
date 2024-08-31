7a52a1bbd22bd7620b7d8da570055f95d199fa5a
Jennifer Rayman <101685011+jennifer-rayman@users.noreply.github.com>
2024-08-30 23:04:11 -0500

    Nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css';
+import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
       <h1>Jennifer's Personal Website!</h1>
       <button>BRAT</button>
       
        
     </div>
   );
+
+
 }
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Services = () => <div>Services Page</div>;
+const Contact = () => <div>Contact Page</div>;
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../CSS/Navigation.css';
+
+const Navigation = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>
+          <Link to="/" className="nav-item">Home</Link>
+        </li>
+        <li>
+          <Link to="/about" className="nav-item">About</Link>
+        </li>
+        <li>
+          <Link to="/services" className="nav-item">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact" className="nav-item">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
